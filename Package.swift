a3e582879ceff7e3c9f3526f4f58e6611d6c01a7
RomanPodymov <podymfrombryansk@yandex.ru>
2021-07-18 02:41:58 +0200

    Fix SPM

--- Package.swift
+++ Package.swift
@@ -3,17 +3,10 @@ import PackageDescription
 
 let package = Package(
     name: "Xcode9to10Preparation",
-    platforms: [
-        .iOS(.v8),
-        .tvOS(.v9)
-    ],
     products: [
         .library(name: "Xcode9to10Preparation", targets: ["Xcode9to10Preparation"])
     ],
     targets: [
         .target(name: "Xcode9to10Preparation", path: "Source"),
-    ],
-    swiftLanguageVersions: [
-        .v4_1
     ]
 )
